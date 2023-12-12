0a19e2dbed80ec2c98983c89241ab5977a73d76d
unknown <unknown@example.com>
2023-12-12 20:54:01 +0800

    增加用户信息全局变量
    增加用户信息全局变量

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,82 +6,85 @@ App({
         } 
     else{
         wx.cloud.init({
-         env:'cloud1-3gro6l2yc2854ff6',
+         env:'cloud1-5gypewt8d4e49289',
          traceUser:true,
         })
         console.log("云服务启动成功")
       }
     },
     globalData: {
-      foodNumber:0,
-      deliveryNumber:0,
-      deliveryArray:[],
-      foodArray:[]
+        avatarUrl:'',
+        username:'',
+        foodNumber:0,
+        deliveryNumber:0,
+        recordArray:[],
+        deliveryArray:[],
+        foodArray:[]
     },
   })
   
-  Page({
+//   Page({
   
-    /**
-     * 页面的初始数据
-     */
-    data: {
+//     /**
+//      * 页面的初始数据
+//      */
+//     data: {
   
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function () {
+//     },
+//     /**
+//      * 生命周期函数--监听页面加载
+//      */
+//     onLoad: function () {
   
-    },
+//     },
   
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+//     /**
+//      * 生命周期函数--监听页面初次渲染完成
+//      */
+//     onReady: function () {
   
-    },
+//     },
   
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+//     /**
+//      * 生命周期函数--监听页面显示
+//      */
+//     onShow: function () {
   
-    },
+//     },
   
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+//     /**
+//      * 生命周期函数--监听页面隐藏
+//      */
+//     onHide: function () {
   
-    },
+//     },
   
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+//     /**
+//      * 生命周期函数--监听页面卸载
+//      */
+//     onUnload: function () {
   
-    },
+//     },
   
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+//     /**
+//      * 页面相关事件处理函数--监听用户下拉动作
+//      */
+//     onPullDownRefresh: function () {
   
-    },
+//     },
   
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+//     /**
+//      * 页面上拉触底事件的处理函数
+//      */
+//     onReachBottom: function () {
   
-    },
+//     },
   
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-    }
-  })
+//     /**
+//      * 用户点击右上角分享
+//      */
+//     onShareAppMessage() {
+//     }
+//   })
 
   
